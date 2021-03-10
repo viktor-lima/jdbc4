f5ca8444898147db418b6940eb507e8b2c9a9f80
viktor-lima <joaolimak10@outlook.com>
2021-03-10 15:40:04 -0300

    atualização de dados

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+		
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"update seller set BaseSalary = BaseSalary + ?"
+					+ "where (DepartmentId = ?)"
+					);
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
